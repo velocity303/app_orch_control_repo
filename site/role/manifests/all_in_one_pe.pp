3f89e985fd93835470006cc42dff21bba9773da2
James E. Jones <james.jones@puppetlabs.com>
2016-02-01 09:51:17 -0600

    remove unnecessary data from control repo

--- site/role/manifests/all_in_one_pe.pp
+++ site/role/manifests/all_in_one_pe.pp
@@ -1,6 +1,5 @@
 class role::all_in_one_pe {
 
   include profile::puppetmaster
-  include profile::git_webhook
 
 }
--- site/role/manifests/all_in_one_pe_2015_2.pp
+++
@@ -1,6 +0,0 @@
-class role::all_in_one_pe_2015_2 {
-
-   include profile::puppetmaster
-   include profile::zack_r10k_webhook
-
-}
